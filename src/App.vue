926a8f87f50755c0bc2a3506ca83a21c9d544a59
Slavik <amazingSevenfix@gmail.com>
2024-08-14 23:18:29 +0500

    quickfix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts">
-
+<script setup >
+  import Header from '/src/components/Header.vue'
+  import Content from '/src/components/Content.vue'
+  import Footer from '/src/components/Footer.vue'
 </script>
 
 <template>
+  <Header />
+  <Content />
+  <Footer />
 
 </template>
 
 <style scoped>
-
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    dynamic content here
+  </div>
+</template>
