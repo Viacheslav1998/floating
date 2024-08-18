346386a437a1b6d3652552a18efe12624123ac6c
Slavik <amazingSevenfix@gmail.com>
2024-08-18 17:38:44 +0500

    feat: header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,21 @@
 </script>
 
 <template>
-  <div class="bg-slate-800 h-[500px] w-full">
-    <h1>Header</h1>
+  <div class="bg-[url('../public/images/header.jpg')] bg-left">
+    <div class="bg-[#838996DE] h-[500px] w-full">
+      <div class="menu">
+        <ul>
+          <li>blog</li>
+          <li>news</li>
+          <li>about us</li>
+          <li>gardens</li>
+          <li>tech</li>
+        </ul>
+      </div>
+      <div class="logo">
+        <h1>Floating</h1>
+        <p>the-news</p>
+      </div>
+    </div>
   </div>
 </template>
