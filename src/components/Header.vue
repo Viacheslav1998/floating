f6bc05dcfcacd52d3b47ad06abd4ee30186782f3
Slavik <amazingSevenfix@gmail.com>
2024-08-19 22:57:51 +0500

    feat work header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,14 +4,16 @@
 <template>
   <div class="bg-[url('../public/images/header.jpg')] bg-left">
     <div class="bg-[#838996DE] h-[500px] w-full">
-      <div class="menu">
-        <ul>
-          <li>blog</li>
-          <li>news</li>
-          <li>about us</li>
-          <li>gardens</li>
-          <li>tech</li>
-        </ul>
+      <div class="flex justify-center">
+        <div class="w-[50%] mt-5">
+          <ul class="flex justify-evenly font-sans text-2xl font-semibold">
+            <li>blog</li>
+            <li>news</li>
+            <li>gardens</li>
+            <li>tech</li>
+            <li>about us</li>
+          </ul>
+        </div>
       </div>
       <div class="logo">
         <h1>Floating</h1>
