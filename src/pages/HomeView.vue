ef5701bd80f56e8f3d5b7ba13439ca285532c7fa
Slavik <amazingSevenfix@gmail.com>
2024-08-24 23:18:28 +0500

    feat: components

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,6 +1,15 @@
-<script>
-export default {
-}
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue';
+
+export default defineComponent({
+  components: {
+    Header, 
+    Footer
+  } 
+}) 
+
 </script>
 
 <template>
