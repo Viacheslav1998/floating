1002ba035e1df2c5873766341320a2bbcdaca062
Slavik <amazingSevenfix@gmail.com>
2024-08-30 04:22:27 +0500

    feat: home css

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -3,8 +3,8 @@
 
 <template>
   <div>
-    <div class="bg-slate-300 py-8 px-4">
-      <h2>Home</h2>
+    <div class="bg-slate-200 py-8 px-4 text-4xl font-semibold text-green-800">
+      <h2>Home:</h2>
     </div>
     <p>you can see it right now</p>
   </div>
