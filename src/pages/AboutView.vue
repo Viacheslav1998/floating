4ae1646b0d5160e1a6fac3a7e9aa48ae3c42c081
Slavik <amazingSevenfix@gmail.com>
2024-09-12 05:43:48 +0500

    feat: preparing, add tamplate

---
+++ src/pages/AboutView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export default {
+    name: 'AboutView'
+  }  
+</script>
+
+<template>
+  <!-- title page -->
+  <div class="flex justify-center">
+    <div class="bg-indigo-900 py-8 px-4 text-4xl font-semibold text-gray-100 relative bottom-10 z-10 w-[400px] flex items-center justify-center">
+      <h2 class="border-b-2 border-gray-100 ">About page</h2>
+    </div>
+  </div>
+  <!-- and title page -->
+</template>
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { createMemoryHistory, createRouter } from "vue-router"
 
 import HomeView from "./pages/HomeView.vue"
+import AboutView from "./pages/AboutView.vue"
 
 const routes = [  
   { path: '/', component: HomeView },  
+  { path: '/about', component: AboutView } 
 ]
 
 const router = createRouter({
