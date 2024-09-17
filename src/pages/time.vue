ad77af2e5e8f5b499842c379166e64dc178fe28f
Slavik <amazingSevenfix@gmail.com>
2024-09-17 21:40:17 +0500

    quickfix

---
+++ src/pages/time.vue
@@ -0,0 +1,27 @@
+<template>
+  
+  <!-- posts max[4elems] -->
+  <div class="grid grid-cols-2 gap-2 bg-white max-w-[800px]">
+    <div class="w-[200px] rounded-lg  h-[400px] bg-slate-400 m-5 p-5">
+      <p>single news here ...</p>
+    </div>    
+
+    <div class="w-[200px] rounded-lg  h-[400px] bg-slate-400 m-5 p-5">
+      <p>single news here ...</p>
+    </div>  
+
+    <div class="w-[200px] rounded-lg  h-[400px] bg-slate-400 m-5 p-5">
+      <p>single news here ...</p>
+    </div>  
+
+    <div class="w-[200px] rounded-lg  h-[400px] bg-slate-400 m-5 p-5">
+      <p>single news here ...</p>
+    </div>    
+  </div>
+  <!-- and posts -->    
+
+
+    
+
+
+</template>
